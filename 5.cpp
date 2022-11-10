86a9d2be31b830a7a1af3ec471579f18d73bda17
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-10 19:49:27 +0300

    12

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+﻿// №0 дано число A=2. Вывести в консоль значение числа А. Затем увеличить число на 4, вывести снова
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 2;
+	cout << a<<endl;
+	cout << a + 4;
+
+}
+
